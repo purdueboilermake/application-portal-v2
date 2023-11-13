096407d256af3488b5f8a992dff0fb918ea94f09
Charlie Sale <softwaresale01@gmail.com>
2023-11-12 20:09:22 -0500

    Add error to login page, show error to user

--- src/login/LoginPage.tsx
+++ src/login/LoginPage.tsx
@@ -1,9 +1,9 @@
-import { Button, Center, Group, Loader, Text} from "@mantine/core";
+import { Alert, Button, Container, Loader, Stack, Text } from "@mantine/core";
 import { GithubAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { ServiceContainer } from "../service/service-container";
 import { useCallback, useMemo, useState } from "react";
-import { notifications } from "@mantine/notifications";
+import { useMediaQuery } from "@mantine/hooks";
 
 export function LoginPage() {
 
@@ -12,6 +12,7 @@ export function LoginPage() {
   const navigator = useNavigate();
   const [loading, setLoading] = useState<boolean>(false);
   const [indicatorMessage, setIndicatorMessage] = useState<string | null>(null);
+  const [loginErrorMsg, setLoginErrorMsg] = useState<string | null>(null);
 
   const loginWithGithub = useCallback(async () => {
     setLoading(true);
@@ -24,30 +25,43 @@ export function LoginPage() {
       const userApplication = await applicationService.getOrCreateUserApplication(user);
       console.log(`Got user application id ${userApplication.id}`);
       navigator(`/application/${userApplication.id}`);
-    } catch (err) {
+    } catch (err: any) {
       setLoading(false);
-      notifications.show({
-        title: 'Error',
-        message: 'Failed to sign in',
-        autoClose: 3000,
-      });
+      setIndicatorMessage(null);
+      if (err instanceof Error) {
+        setLoginErrorMsg(`${err.name} - ${err.message}`);
+      } else {
+        setLoginErrorMsg(err?.toString?.call(err) ?? 'Unknown error');
+      }
     }
   }, [auth, navigator, applicationService]);
 
+
+  const isPhoneDevice = useMediaQuery('(max-width: 575px)');
   return (
-    <Center h={100}>
-      <Group>
+    <Container pt={isPhoneDevice ? 100 : undefined}>
+      <Stack>
         <Button onClick={loginWithGithub}>
-          Login with GitHub
-        </Button>
-        { loading &&
-          <Loader />
-        }
-        { indicatorMessage &&
-          <Text>{indicatorMessage}</Text>
-        }
-      </Group>
-    </Center>
+            Login with GitHub
+          </Button>
+          { loading &&
+            <Loader />
+          }
+          { indicatorMessage &&
+            <Text>{indicatorMessage}</Text>
+          }
+          { loginErrorMsg &&
+            <Alert variant="filled" title="Login Error" color="red">
+              <p>
+                An error occured while trying to login. Please try again.
+              </p>
+              <code>
+                {loginErrorMsg}
+              </code>
+            </Alert>
+          }
+      </Stack>
+    </Container>
   )
 }
 
